0eca4c78dc95709441626029b59706ae9e43279a
Bill Shetti <billshetti@gmail.com>
2020-07-21 15:31:05 -0700

    first commit

---
+++ environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash         
+
+export ORDER_DB_HOST="localhost"
+export ORDER_DB_PORT="5432"
+export ORDER_DB_PASSWORD="password"
+export ORDER_DB_USERNAME="postgres"
+export PGPASSWORD="password"
+export ORDER_AUTH_DB="postgres"
+export PAYMENT_HOST="localhost"
+export PAYMENT_PORT="9000"
+export AUTH_MODE="0"
